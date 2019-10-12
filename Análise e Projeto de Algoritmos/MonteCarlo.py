870acfe7275a67495fe8975c2f10149884aab0a6
Arthur Magalhães <artmgsil140@gmail.com>
2019-10-12 15:21:35 -0300

    Agentes Lógicos.

--- Análise e Projeto de Algoritmos/MonteCarlo.py
+++ Análise e Projeto de Algoritmos/MonteCarlo.py
@@ -2,7 +2,7 @@ import re
 import random
 
 
-def match(poly1, poly2):
+def match(poly1, poly2, factor=1):
     # A primeira entrada é um dicionário com as chaves da forma x^n, para n natural, 
     # e os valores sendo os respectivos coeficientes, do primeiro polinômio.
     # A segunda entrada é uma lista com as raízes do segundo polinônmio.
@@ -16,11 +16,11 @@ def match(poly1, poly2):
         grau = len(poly2)
         alfa = random.randint(1, 100 * grau)   
         res1 = 0
-        res2 = 1
+        res2 = factor # Coeficiente dominante.
         valores.append(alfa)
         
         for k, v in poly1.items():
-            exp = int(re.search('[0-9]+', k, re.IGNORECASE).group(0))
+            exp = int(re.search('[0-9]+', k, re.IGNORECASE).group(0)) # Seleciona o grau do monômio.
             res1 += v * alfa ** exp
     
         for r in poly2:
@@ -52,7 +52,7 @@ def polynomyParsev2():
     poly1 = {} # Dicionário
     poly2 = [] # Lista
 	
-    for i in range(n, -1, -1):
+    for i in range(n, -1, -1): # Preenche um dicionário tendo a expressão "x^", seguida pelo grau do monômio, como chave e o respectivo coeficiente, do primeiro polinômio, como valor.
         if (i < n) and (i > 0):
             poly1['x^{}'.format(i)] = int(input('Digite o coeficiente de grau {} do primeiro polinômio: '.format(i)))
         elif i == n:
@@ -61,13 +61,61 @@ def polynomyParsev2():
             poly1['x^{}'.format(i)] = int(input('Digite o termo independente do primeiro polinômio: '))
     
     print('\n')
-    for i in range(1, n + 1):
+    for i in range(1, n + 1): # Preenche uma lista com as raízes do segundo polinômio.
         poly2.append(int(input('Digite a {}ª raíz do segundo polinômio: '.format(i))))
+	
+    factor = 0
+    if input('Deseja multiplicar por algum fator? [s/n] ') == 's':
+        while factor == 0:
+            factor = int(input('Digite o fator (não nulo): '))
+            if factor == 0:
+                print('Valor inválido!! Digite novamente: ')
+    else:
+        factor = 1
     
-    return poly1, poly2
+    return poly1, poly2, factor
 
 
-p1, p2 = polynomyParsev2()
-d, l = match(p1, p2)
+def showPolynomys(poly1, poly2, factor=1):
+    extended = ''
+    factored = ''
+    
+    for k, v in poly1.items():
+        if int(re.sub('\D', '', k)) > 1: # Remove os caracteres não numéricos.
+            extended += (str(v)+ k + ' + ') # Concatenação dos monômios.
+        elif int(re.sub('\D', '', k)) == 1:
+            extended += (str(v) + 'x + ')
+        else:
+            extended += str(v)
+    
+    if factor != 1:
+        if poly2[0] > 0:
+            factored += '({}x - {})'.format(factor, factor*poly2[0])
+        elif poly2[0] < 0:
+            factored += '({}x + {})'.format(factor, abs(factor*poly2[0]))
+        else:
+            factored += '({}x)'.format(factor)
+    else:
+        if poly2[0] > 0:
+    	    factored += '(x - {})'.format(factor*poly2[0])
+        elif poly2[0] < 0:
+            factored += '(x + {})'.format(abs(factor*poly2[0]))
+        else:
+    	    factored += '(x)'
+    
+    for r in poly2[1:]: # Começa a iteração à partir da segunda raíz.
+        if r > 0:
+    	    factored += '(x - {})'.format(factor*r)
+        elif r < 0:
+            factored += '(x + {})'.format(abs(factor*r))
+        else:
+            factored += '(x)'
+       
+    print('O primeiro polinômio é ' + extended)
+    print('O segundo polinômio é ' + factored, end='\n\n')
 
+
+p1, p2, dom = polynomyParsev2()
+d, l = match(p1, p2, dom)
+showPolynomys(p1, p2, dom)
 print(d, l)
